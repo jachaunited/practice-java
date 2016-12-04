c365f5876e5d2efd729fb00b99f1f1d945ef2e13
6470B <6470B@DESKTOP-84SGVFM>
2016-12-05 02:07:28 +0800

    test

--- java_practice/src/janelle/ConvertString.java
+++ java_practice/src/janelle/ConvertString.java
@@ -1,4 +1,4 @@
-package java_practice;
+package janelle;
 
 public class ConvertString {
 	String[] letterArr = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
