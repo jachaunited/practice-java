95fb99d42ba4e6f7598fefb575270d44d2e01f62
Chamver <Chamver@HOMEPC>
2016-12-07 02:28:50 +0800

    Update on multiplicationTable

--- java_practice/src/john/nokia/src/nokia/multiplicationTable.java
+++ java_practice/src/john/nokia/src/nokia/multiplicationTable.java
@@ -5,12 +5,29 @@ import java.util.Scanner;
 public class multiplicationTable {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in)
+		Scanner in = new Scanner(System.in);
 		int number = in.nextInt();
+		int[][] table = new int[number][number];
+		
+		for(int index = 0; index < number; index++) {
+			table[0][index] = index + 1;
+			table[index][0] = index + 1;
+		}
+		
+		for(int row = 1; row < number; row++) {
+			for(int column = 1; column < number; column++) {
+				table[row][column] = table[row][0]*table[0][column];
+			}
+		}
 		
 		for(int row = 0; row < number; row++) {
 			for(int column = 0; column < number; column++) {
-				
+				if(column % number == 0){
+					System.out.println();
+					System.out.print(table[row][column] + "\t");
+				} else {
+					System.out.print(table[row][column] + "\t");
+				}
 			}
 		}
 		
